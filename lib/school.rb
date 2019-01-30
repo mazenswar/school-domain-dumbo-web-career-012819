d77f098ecf72e59c2747355ad51fb993cdf4b1af
mazenswar <mazenswar@protonmail.com>
2019-01-30 12:07:36 -0500

    school domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+require "pry"
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] ? @roster[grade] << student : @roster[grade] = [student]
+  end
+
+  def grade(grade_number)
+    @roster[grade_number]
+  end
+
+  def sort
+    @roster.sort_by { |k,v| k }.to_h
+    @roster.each_pair { |k,v| @roster[k] = v.sort }
+  end
+
+end
